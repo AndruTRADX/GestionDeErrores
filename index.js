4b296c2ece7f94fa4ace663970177aef3f669a70
andrutradx <andrudeluxe@gmail.com>
2024-06-13 09:37:31 -0500

    Error with the <a> label

--- index.js
+++ index.js
@@ -2,7 +2,12 @@ const searcher = document.querySelector('.main-scene-searcher');
 
 searcher.addEventListener('submit', (e) => {
   e.preventDefault();
-  const inputValue = e.target.searcher.value;
+  const inputValue = e.target.searcher.value.trim();
+
+  if (inputValue === "") {
+    alert("Please fill the field.");
+    return
+  }
 
   alert(inputValue);
 });
--- submit.js
+++ submit.js
@@ -3,10 +3,20 @@ const formSubmit = document.querySelector('.submit-form');
 formSubmit.addEventListener('submit', (e) => {
   e.preventDefault();
   
-  const type = e.target.type.value;
-  const description = e.target.description.value;
-  const steps = e.target.steps.value;
-  const impact = e.target.impact.value;
+  const type = e.target.type.value.trim();
+  const description = e.target.description.value.trim();
+  const steps = e.target.steps.value.trim();
+  const impact = e.target.impact.value.trim();
 
-  alert(inputValue);
+  if (type === "" || description === "" || steps === "" || impact === "") {
+    alert("All fields must be filled out");
+    return;
+  }
+
+  e.target.type.value = "";
+  e.target.description.value = "";
+  e.target.steps.value = "";
+  e.target.impact.value = "";
+
+  alert("Ticket has been submitted successfully!");
 });
